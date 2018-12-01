7f5afe3077890bafb6ed1dd8cda6e4c44996f6a7
Meghna Gupta <meghnagupta64@gmail.com>
2018-12-01 20:01:04 +0530

    Activity committed

--- app/src/main/java/com/example/waste_classification/HomeActivity.java
+++ app/src/main/java/com/example/waste_classification/HomeActivity.java
@@ -1,13 +1,61 @@
 package com.example.waste_classification;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView camera = (ImageView)findViewById(R.id.cameraButton);
+        ImageView upload = findViewById(R.id.uploadButton);
+        ImageView infof = findViewById(R.id.infoButton);
+        ImageView help = findViewById(R.id.helplineButton);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, UploadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        infof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, HelpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
+
 }
