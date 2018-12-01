07d428a7118d0cc8c51c20b09152be4dc3abe2d2
Meghna Gupta <meghnagupta64@gmail.com>
2018-12-01 19:18:27 +0530

    Initial commit

--- app/src/main/java/com/example/waste_classification/MainActivity.java
+++ app/src/main/java/com/example/waste_classification/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.waste_classification;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
